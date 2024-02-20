c001b3dfcbd9cdee504a010fb8d11bf2d930bf93
Shant Dashjian <shantgdashjian@gmail.com>
2024-02-20 23:15:36 +0400

    Add speaking functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 
 const translateBtn = document.getElementById('translate-btn')
+const speakBtn = document.getElementById('speak-btn')
 const englishTextArea = document.getElementById('english-text-area')
 const germanTextArea = document.getElementById('german-text-area')
 
@@ -8,6 +9,8 @@ const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   dangerouslyAllowBrowser: true
 });
+
+const elevenLabsApiKey = import.meta.env.VITE_ELEVEN_LABS_API_KEY
 
 translateBtn.addEventListener('click', async () => {
   const englishText = englishTextArea.value 
@@ -25,4 +28,27 @@ translateBtn.addEventListener('click', async () => {
     ],
   });
   germanTextArea.value = response.choices[0].message.content
+})
+
+speakBtn.addEventListener('click', async () => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Accept': 'audio/mpeg',
+      'xi-api-key': elevenLabsApiKey,
+      'Content-Type': 'application/json'
+    },
+    body: `{"model_id":"eleven_multilingual_v2","text": "${germanTextArea.value}"}`
+  };
+
+  try {
+    const response = await fetch('https://api.elevenlabs.io/v1/text-to-speech/pNInz6obpgDQGcFmaJgB?output_format=mp3_22050_32', options)
+    const mp3Data = await response.blob();
+    const mp3Url = URL.createObjectURL(mp3Data);
+    const audio = new Audio(mp3Url);
+    audio.play();
+  }
+  catch(err) {
+    console.error(err);
+  }
 })
